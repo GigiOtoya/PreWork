ea275c241c2262364427d8f2fc0c5f618e7f934a
Luigi Otoya <Luigi.Otoya@gmail.com>
2022-08-20 17:55:13 -0400

    changes to colors

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -5,11 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val switchCompat01 = findViewById<SwitchCompat>(R.id.switchCompat01)
+        switchCompat01.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
         val button = findViewById<Button>(R.id.helloButton)
         button.setOnClickListener {
             Log.v("Hello world", "Button clicked!")
